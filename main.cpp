29c55ba771f3edda25c98124239b486d59ce48ab
Johannes Stratmann <sn2010@onlinehome.de>
2021-11-03 15:09:13 +0100

    spi 33 MHz 16 bit

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "mbed.h"
+#include "stm32h7xx_ll_spi.h"
 
-#define SPI_3WIRE
+// #define SPI_3WIRE
 #define SPI_4WIRE
 
 static DigitalOut user_led(LED1, 1);
@@ -46,18 +47,43 @@ static void MX_SPI2_Init(SPI_HandleTypeDef &hspi2)
     }
 }
 
+static SPI_TypeDef *staticSPI;
+
 class MySPI : public SPI {
     public:
     MySPI(PinName mosi, PinName miso, PinName sclk, PinName ssel = NC) :
         SPI(mosi, miso, sclk, ssel)
-    {}
+    {
+        struct spi_s *spiobj = (( struct spi_s *)(&_peripheral->spi));
+    }
 
     void reinit() {
+        spi_t *obj = &_peripheral->spi;
         struct spi_s *spiobj = (( struct spi_s *)(&_peripheral->spi));
         SPI_HandleTypeDef *handle = &(spiobj->handle);
 
-        MX_SPI2_Init(*handle);
+        staticSPI = ((SPI_TypeDef *)(obj->spi.spi));
+
+        LL_SPI_StartMasterTransfer(staticSPI);
+
+        //MX_SPI2_Init(*handle);
     }
+
+    static inline void spi_transmit(uint32_t data)
+    {
+        LL_SPI_TransmitData16(staticSPI, data);
+        while(!LL_SPI_IsActiveFlag_TXP(staticSPI)) {};
+//        LL_SPI_ReceiveData8(staticSPI); /* dummy read-access to clear SPI_SR_RXWNE */
+    }
+
+    static inline uint32_t spi_receive(uint32_t data)
+    {
+        LL_SPI_TransmitData8(staticSPI, data);
+        while(!LL_SPI_IsActiveFlag_TXP(staticSPI)) {};
+        while(!LL_SPI_IsActiveFlag_RXWNE(staticSPI)) {};
+        return LL_SPI_ReceiveData8(staticSPI);
+    }
+
 };
 #else
 #define MySPI SPI
@@ -89,12 +115,18 @@ int main()
 
     size_t tx_word_len;
     size_t rx_word_len;
+    const uint8_t tx_data8[8] = {0x11, 0x22, 0x33, 0x44}; // allocate extra memory to prevent "out of range" error
     const uint16_t tx_data[8] = {0x1122, 0x3344}; // allocate extra memory to prevent "out of range" error
     uint16_t rx_data[8] = {0};
 
     wait_us(2000);
 
-    constexpr uint spi_freq = 100'000'000;
+    constexpr uint spi_freq = 40'000'000;
+
+            MySPI spi(mosi, miso, sclk);
+            spi.format(16, 0);
+            spi.frequency(spi_freq);
+            spi.reinit();
 
     while (true) {
         //user_led = 1;
@@ -137,30 +169,32 @@ int main()
         tx_word_len = 2;
         rx_word_len = 2;
         {
-            MySPI spi(mosi, miso, sclk);
-            spi.format(16, 0);
-            spi.frequency(spi_freq);
-            //spi.reinit();
 
             wait_us(64);
 
             ssel_out = 0;
-
             // transfer by 1 word per SPI::write call
             for (size_t i = 0; i < tx_word_len; i++) {
-                spi.write(tx_data[i]);
+                spi.spi_transmit(tx_data[i]);
             }
             wait_us(64);
 
-            // transfer by bulk SPI::write call
-            spi.write((const char *)tx_data, tx_word_len * sizeof(*tx_data),
-                      (char *)rx_data, rx_word_len * sizeof(*rx_data));
-            wait_us(64);
+
+            // // transfer by 1 word per SPI::write call
+            // for (size_t i = 0; i < tx_word_len; i++) {
+            //     spi.write(tx_data[i]);
+            // }
+            // wait_us(64);
+
+            // // transfer by bulk SPI::write call
+            // spi.write((const char *)tx_data, tx_word_len * sizeof(*tx_data),
+            //           (char *)rx_data, rx_word_len * sizeof(*rx_data));
+            // wait_us(64);
             
-            // asynchronous transfer
-            spi.transfer(tx_data, tx_word_len * sizeof(*tx_data),
-                         rx_data, rx_word_len * sizeof(*rx_data), spiSendDone);
-            wait_us(64);
+            // // asynchronous transfer
+            // spi.transfer(tx_data, tx_word_len * sizeof(*tx_data),
+            //              rx_data, rx_word_len * sizeof(*rx_data), spiSendDone);
+            // wait_us(64);
 
             ssel_out = 1;
         }
